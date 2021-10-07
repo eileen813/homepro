a6a3b226e615111204ebd04e2811dc13a7371e50
Eileen Olivera <eileenolivera@hotmail.com>
2021-10-07 13:59:19 -0400

    setup client-side base url for deployment

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,14 +1,9 @@
 import axios from "axios";
 
-// once deployed, follow this block of code, and remove
-// the current const baseUrl and const api.
-// const api = axios.create({
-//   baseURL: process.env.NODE_ENV === 'production'
-//       ? 'https://AppNameHere/.herokuapp.com/api'
-//       : 'http://localhost:3000/api'
-// })
-
-const baseUrl = "https://localhost:3000";
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://homepro-api.herokuapp.com/"
+    : "http://localhost:3000";
 
 const api = axios.create({
   baseURL: baseUrl,
