21909e4ab179ca350b373fd04f7da5144aa3fc5c
Eileen Olivera <eileenolivera@hotmail.com>
2021-10-09 17:29:52 -0400

    updated nav styling

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,23 +1,25 @@
 import { Link } from "react-router-dom";
+import "./Nav.css";
 
 export default function Nav(props) {
   return (
     <div>
-      <h1>this is my Nav component</h1>
-      {props.currentUser ? (
-        <div>
-          <p>{props.currentUser.username}</p>
-          <button onClick={props.handleLogout}>Logout</button>
-        </div>
-      ) : (
-        <Link to="/sign-up">Sign Up</Link>
-      )}
-      <Link to="/login">Login</Link>
-      {props.currentUser && (
-        <div>
-          <Link to="/projects">Projects</Link>
-        </div>
-      )}
+      <ul>
+        {props.currentUser ? (
+          <div>
+            <p>{props.currentUser.username}</p>
+            <button onClick={props.handleLogout}>Logout</button>
+          </div>
+        ) : (
+          <Link to="/sign-up">Sign Up</Link>
+        )}
+        <Link to="/login">Login</Link>
+        {props.currentUser && (
+          <div>
+            <Link to="/projects">Projects</Link>
+          </div>
+        )}
+      </ul>
     </div>
   );
 }
